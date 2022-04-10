9b0b8218d0a0ef94d8386c7ee9536d7045073071
xiaoan001 <1270949404@qq.com>
2022-04-10 22:40:12 +0800

    进阶指针

--- game.h
+++
@@ -1,6 +0,0 @@
-#define ROW 10
-#define COL 10
-//ÉùÃ÷
-#include <stdio.h>
-void InitBoard(int board[ROW][COL],int row,int col);
-void DisplayBoard(char board[ROW][COL], int row, int col);
--- test.4.7.cpp
+++ test.4.7.cpp
@@ -1,117 +1,160 @@
-﻿ #include <stdio.h>
-//int main()
+﻿//指针进阶
+#include <stdio.h>
+//void test(int arr[])
 //{
-	/*printf("%d\n", sizeof(char*));
-	printf("%d\n", sizeof(float*));
-	printf("%d\n", sizeof(int*));
-	printf("%d\n", sizeof(double*));*/
-	//int a = 0x11223344;
-	//int*pa=&a;
-	//int b = 0x55667788;
-	//int* pb = &b;
-	//pb = 0;
-	////char*pc = &a;
-	//printf("%p\n", pa);
-	//printf("%p\n", pb);
-	////printf("%p\n", pc);
-	/*printf("%d\n", sizeof(char*));
-	printf("%d\n", sizeof(float*));
-	printf("%d\n", sizeof(int*));
-	printf("%d\n", sizeof(double*));
-	printf("%d\n", sizeof(char));
-	printf("%d\n", sizeof(float));
-	printf("%d\n", sizeof(int));
-	printf("%d\n", sizeof(double));*/
-	//指针类型决定了：指针走一步走多远（指针的步长
-	/*int* p; p + 1-- > 4
-		char* p;p+1-->1*/
-	//int arr[10] = { 0 };
-	//int*p=arr;//数组名-收元素地址
-	//int i = 0;
-	//for (i = 0; i < 10; i++)
-	//{
-	//	  *(p + i) = 1;
-	//}
-	//野指针
-	//局部变量不初始化，默认是随机值
-	//指针越界
-
-
-
 //
+//    int sz = sizeof(arr) / sizeof(arr[0]);
+//    printf("%d\n", sz);
+//}
+//int main()
+//{
+//    
+//    int arr[10] = { 0 };
+//    test(arr);
+//    return 0;
+//}
+//int main()
+//{
+//	/*char ch = 'w';
+//	char* pc = &ch;*/
+//	char arr[] = "abcdef";
+//	char* pc = arr;
+//	
+//	printf("%s\n", arr);
+//	printf("%s\n", pc);
 //	return 0;
 //}
 //int main()
 //{
-//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-//	int i = 0;
-//	int sz = sizeof(arr)/sizeof(arr[0]);
-//	int* p = arr;
-//	for (i = 0; i < sz; i++)
-//	{
-//		printf("%d", *p);
-//		p++;
-//	}
-//}
-//指针减指针得到的是中间元素个数必须是同一个数组
-//int main()
-//{
-//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-//	printf("%d\n", &arr[9] - &arr[0]);
+//	const char* p = "abcdef";
+//	printf("%s\n", p);	
 //	return 0;
 //}
-//int my_strlen(char* str)
+//segentfault   网站
+//int main()
 //{
-//	char* start = str;
-//	char* end = str;
-//	while (*end != '0');
+//	char arr1[]="abcdef";
+//	char arr2[]="abcdef";
+//	char* p1 = "abcdef";
+//	char* p2 = "abcdef";
+//	if (arr1 == arr2)
 //	{
-//		end++;
+//		printf("hehe\n");
 //	}
-//	return end - start;
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
 //}
 //int main()
 //{
-//	//strlen--求字符串长度
-//	//递归--模拟实现了strlen-计数器的方式1，
-//	char arr[] = "bit";
-//	int len = my_strlen(arr);
-//	printf("%d\n", len);
+//	char arr1[] = "abcdef";
+//	char arr2[] = "abcdef";
+//	const char* p1 = "abcdef";
+//	const char* p2 = "abcdef";
+//	if (p1==p2)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
 //}
+//指针数组
 //int main()
 //{
 //	int arr[10] = { 0 };
-//	printf("%p\n", arr);//地址-首元素的地址
-//	printf("%p\n", &arr[0]);
-//	printf("%p\n", &arr);
-//	//&arr-&数组名-数组名不是首元素地址--数组表示整个数组-&数组名 
-//	//取出的是整个数组的地址
-//    //sizeof（arr)表示计算整个数组大小
-//	printf("%p\n", arr+1);//地址-首元素的地址
-//	printf("%p\n", &arr[0]+1);
-//	printf("%p\n", &arr+1);
-//	printf("%p\n", *(arr));//地址-首元素的地址
-//	printf("%p\n", *(&arr[0]));
-//	printf("%p\n", *(&arr));
+//	char ch[5] = { 0 };
+//	int* parr[4];//存放整形指针的数组-指针数组
+//	char* pch[5];//存放字符指针的数组-指针数组
+//
 //
 //}
-//指针数组---数组--存放指针的数组
-//数组指针---指针
-// int    arr[10]
-// int*   arr[10]---指针数组
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int d = 40;
+//	int* arr[4] = { &a,&b,&c,&d };
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d ", *arr[i]);
+//	}
+//}
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 2,3,4,5,6 };
+//	int arr3[] = { 3,4,5,6,7 };
+//    int* parr[] = { arr1,arr2,arr3 };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%d", * (parr[i]+j));
+//		}
+//	}
+//	return 0;
+//}
+//数组指针--指针
+//int main()
+//{
+//	int* p = NULL;//p是整形指针-指向整形的指针
+//	char* pc =NULL;//pc是字符指针-指向字符的指针
+//	//数组指针-指向数组的指针--存放数组的地址
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int (*p)[10] = &arr;  &arr取的是整个数组的元素
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int(*pa)[10] = &arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", (*pa)[i]);
+//	}
+//	return 0;
+//}
+void print1(int arr[3][5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x;i++ )
+	{
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+}
+void print2(int (*p)[5], int x, int y)
+{
+	int i = 0;
+	for (i = 0; i < x; i++)
+	{ 
+		int j = 0;
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", *(*(p + i)+j));
+		}
+	}
+}
 int main()
 {
-	int a = 10;
-	int b = 20;
-	int c = 30;
-	int* arr[3] = { &a,&b,&c };//指针数组
-	int i = 0;
-	for (i = 0; i < 3; i++)
-	{
-		printf("%d\n", *(arr[i]));
-		
-	}
-	return 0;
-
+	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+	print1(arr, 3, 5);
+	print2(arr, 3, 5);
+	return  0;
 }
 
